e728feb2de03f38e81ac9da5e63fa35b87ed53e8
junseongwoo <wnstjd4852@gmail.com>
2021-08-20 10:02:38 +0900

    이미지 크롤링 코드

--- image_crawling.py
+++ image_crawling.py
@@ -10,14 +10,10 @@ elem.send_keys("크롤링할 사진 키워드")
 elem.send_keys(Keys.RETURN)
 
 SCROLL_PAUSE_TIME = 1
-# Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
 while True:
-    # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # Wait to load page
     time.sleep(SCROLL_PAUSE_TIME)
-    # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         try:
